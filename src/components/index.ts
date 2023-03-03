1acf0328e9e6956d8fb9ca76f58246fdee2fdb18
Daniel Nguyen <danielnguyen5201@gmail.com>
2023-03-04 06:05:08 +0700

    init project

---
+++ src/components/index.ts
@@ -0,0 +1,24 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas/index.js';
+import Hero from './Hero.js';
+import Navbar from './Navbar';
+import About from './About.js';
+import Tech from './Tech';
+import Experience from './Experience.js';
+import Works from './Works';
+import Feedbacks from './Feedbacks.js';
+import Contact from './Contact.js';
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+}
